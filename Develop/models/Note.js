b79c73c9a82adec3effeb0f8c4de7789062779ef
Jason Manning <jasonkylemanning@gmail.com>
2021-05-15 13:48:32 -0400

    update Note.js file with Note object

--- Develop/models/Note.js
+++ Develop/models/Note.js
@@ -5,6 +5,13 @@ class Note extends Model {}
 
 Note.init(
     {
-        
-    }
-)
+        note_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+    },
+);
