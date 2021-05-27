afe64123260d3566908cbb4adb092acb539a3045
Jason Manning <jasonkylemanning@gmail.com>
2021-05-27 17:56:51 -0400

    heroku port correction

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
